df5ca2ed14aa16e5d73ab667d75801f92094211c
SSBun <caishilin@yahoo.com>
2017-09-19 16:33:08 +0800

    Initial Commit

---
+++ FlyCoding/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FlyCoding
+//
+//  Created by SSBun on 19/09/2017.
+//  Copyright © 2017 SSBun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FlyCoding/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FlyCoding
+//
+//  Created by SSBun on 19/09/2017.
+//  Copyright © 2017 SSBun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
