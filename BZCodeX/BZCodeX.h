9f1769cb4b30ba5e4433add496855cca91cf0a88
蔡士林 <caishilin@babyfs.com>
2019-07-26 14:24:19 +0800

    Add BZCodeX framework

---
+++ BZCodeX/BZCodeX.h
@@ -0,0 +1,19 @@
+//
+//  BZCodeX.h
+//  BZCodeX
+//
+//  Created by caishilin on 2019/7/26.
+//  Copyright © 2019 SSBun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BZCodeX.
+FOUNDATION_EXPORT double BZCodeXVersionNumber;
+
+//! Project version string for BZCodeX.
+FOUNDATION_EXPORT const unsigned char BZCodeXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BZCodeX/PublicHeader.h>
+
+
