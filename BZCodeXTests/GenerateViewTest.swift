4912b02f9990128c3ebbb81658cba99041e583e1
蔡士林 <caishilin@babyfs.cn>
2020-01-17 11:46:12 +0800

    Fixup  bug : `make` command hasn't any response.

---
+++ BZCodeXTests/GenerateViewTest.swift
@@ -0,0 +1,39 @@
+//
+//  GenerateViewTest.swift
+//  BZCodeXTests
+//
+//  Created by caishilin on 2020/1/17.
+//  Copyright © 2020 SSBun. All rights reserved.
+//
+
+
+import XCTest
+@testable import BZCodeX
+
+class GenerateViewTest: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_swift_make_uiview() {
+        let code = "make(UIView,testView)"
+        let answer =
+        """
+let testView = UIView()
+testView.backgroundColor = <#color#>
+<#superView#>.addSubview(testView)
+
+
+"""
+        if let snip = BaseSnip.init(label: code, spaceCount: 0, codeType: .swift) {
+            XCTAssertEqual(answer, snip.code)
+        } else {
+            XCTFail("Analyze code failure.")
+        }
+    }
+}
